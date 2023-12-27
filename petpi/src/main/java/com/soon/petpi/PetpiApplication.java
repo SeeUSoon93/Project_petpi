4d40b8274801aa01e62c7c201016f2d6994288ac
김군순 <rlarnstns@gmail.com>
2023-12-27 13:58:31 +0900

    워크스페이스 다시

---
+++ petpi/src/main/java/com/soon/petpi/PetpiApplication.java
@@ -0,0 +1,13 @@
+package com.soon.petpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetpiApplication.class, args);
+	}
+
+}

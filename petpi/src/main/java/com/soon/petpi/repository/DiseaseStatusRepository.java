117ace15853ca1dd41dce8d3d1978c250b2aa339
최윤석 <97040581+Yundae203@users.noreply.github.com>
2024-01-03 11:30:59 +0900

    Revert "Feature/#23"

--- petpi/src/main/java/com/soon/petpi/repository/DiseaseStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.soon.petpi.repository;
-
-import com.soon.petpi.model.entity.DiseaseStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiseaseStatusRepository extends JpaRepository<DiseaseStatus, Long> {
-}
--- petpi/src/main/java/com/soon/petpi/repository/HealthStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.soon.petpi.repository;
-
-import com.soon.petpi.model.entity.HealthStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HealthStatusRepository extends JpaRepository<HealthStatus, Long> {
-}

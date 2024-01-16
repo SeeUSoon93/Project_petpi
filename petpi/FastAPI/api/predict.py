626cf35267592470359237abe5305b90a81df05a
김군순 <rlarnstns@gmail.com>
2024-01-16 18:49:25 +0900

    FastAPI 부분 끝

--- petpi/FastAPI/api/predict.py
+++ petpi/FastAPI/api/predict.py
@@ -7,7 +7,7 @@ from tensorflow.keras.models import load_model
 unet_model_path = ['model/unet01.h5', 'model/unet02.h5', 'model/unet03.h5']
 unet_models = [load_model(path)for path in unet_model_path]
 
-inception_model = load_model('model/all_model_01.h5')
+inception_model = load_model('model/final2_01.h5')
 
 class_labels = {
         0: 'A1_구진/플라크',
